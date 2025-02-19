865b8246a96dcaed5587c167444966e7fd4c3f67
Kevin Freistroffer <Kevin.freistroffer@gmail.com>
2025-02-18 19:11:55 -0800

    root directory

--- app/build.sh
+++ app/build.sh
@@ -6,7 +6,7 @@ pip freeze > requirements.txt
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python ./manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python ./manage.py migrate
