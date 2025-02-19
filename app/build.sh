7d73444f5144a8e7f7639a9264a3862354ce983b
Kevin Freistroffer <Kevin.freistroffer@gmail.com>
2025-02-18 19:23:22 -0800

    Update build.sh

--- app/build.sh
+++ app/build.sh
@@ -4,7 +4,11 @@ set -o errexit
 
 # Debug: Print current directory
 pwd
+ls
 ls -la
+
+cd django_machine_learning/app
+ls
 
 pip freeze > requirements.txt
 # Modify this line as needed for your package manager (pip, poetry, etc.)
